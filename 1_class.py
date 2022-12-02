490983fc2ec5befcdcc742de2284e90fab47b79e
Ivan Gerasimenko <noleen@ya.ru>
2022-12-02 17:46:37 +0300

    button

--- 1_class.py
+++ 1_class.py
@@ -1,2 +1,27 @@
-class LittleBel:
-    df = 0
+# class LittleBel:
+#     def sound (self):
+#         print('ding')
+#
+#
+# a = LittleBel()
+# a.sound()
+
+
+class Button:
+    count = 0
+    def click (self):
+        Button.count += 1
+    def click_count(self):
+        return Button.count
+
+    def reset(self):
+        Button.count = 0
+
+
+b = Button()
+b.click_count()
+b.click()
+b.click()
+a = Button()
+a.click()
+print(b.click_count())
