17f99300d75b6cb9481a7c70faa5caa657ea3caa
Ivan Gerasimenko <noleen@ya.ru>
2023-01-23 19:57:25 +0300

    1

--- 1_class.py
+++ 1_class.py
@@ -101,40 +101,101 @@
 # Метод add_r принимает целое число - вес, положенный на правую чашу. Метод add_l - на левую.
 # Метод result выводит "=", если чаши равны, "R" если в правой больше, "L" если в левой больше
 
-class Balance:
+# class Balance:
+#
+#     weigh_R = 0
+#     weigh_L = 0
+#
+#     def add_r(self,w):
+#         self.weigh_R += w # тут и далее пишем self.weigh_R вместо Balance.weigh_R, чтоб новые значения записывались не в атрибуты класса, а отдельно в атрибуты объекта, чтоб при создании нового объекта атрибуты обнулялись!
+#
+#     def add_l(self,w):
+#         self.weigh_L += w
+#
+#     def result(self):
+#         r = self.weigh_R
+#         l = self.weigh_L
+#         if r == l:
+#             print('=')
+#         elif r > l:
+#             print('R')
+#         elif r < l:
+#             print('L')
+#
+# b = Balance()
+# b.result()
+# b.add_r(10)
+# b.add_l(7)
+# b.add_l(5)
+# b.result()
+#
+# b2 = Balance()
+# b2.result()
+# b2.add_l(10)
+# b2.add_r(7)
+# b2.add_r(5)
+# b2.result()
+########################################
+########################################
 
-    weigh_R = 0
-    weigh_L = 0
+# напишите класс BigBell который при вызове метода sound печатает попеременно ding и dong
 
-    def add_r(self,w):
-        self.weigh_R += w # тут и далее пишем self.weigh_R вместо Balance.weigh_R, чтоб новые значения записывались не в атрибуты класса, а отдельно в атрибуты объекта, чтоб при создании нового объекта атрибуты обнулялись!
+# class BigBell:
+#     check = 1
+#
+#     # def sound(self):
+#     #     if BigBell.check:
+#     #         print('ding')
+#     #         BigBell.check = 0
+#     #     else:
+#     #         print('dong')
+#     #         BigBell.check = 1
 
-    def add_l(self,w):
-        self.weigh_L += w
-
-    def result(self):
-        r = self.weigh_R
-        l = self.weigh_L
-        if r == l:
-            print('=')
-        elif r > l:
-            print('R')
-        elif r < l:
-            print('L')
-
-b = Balance()
-b.result()
-b.add_r(10)
-b.add_l(7)
-b.add_l(5)
-b.result()
-
-b2 = Balance()
-b2.result()
-b2.add_l(10)
-b2.add_r(7)
-b2.add_r(5)
-b2.result()
+#       # ЛУчшЕ ТАК:
+#     def sound(self):
+#         if self.check:
+#             print('ding')
+#             self.check = 0
+#         else:
+#             print('dong')
+#             self.check = 1
+#
+# b = BigBell()
+#
+# b2 = BigBell()
+# b2.sound()
+# b.sound()
+# b2.sound()
+# b2.sound()
 
 
+################################################################################
+################################################################################
 
+class MinMaxWordFinder:
+    def __init__(self):
+        self.words = []
+
+    def add_sentenses(self, line: str):
+        self.words += line.split()
+
+    def short(self):
+        min_ = len(sorted(self.words, key=len)[0])
+        return list(filter(lambda x: len (x) == min_, self.words))
+
+    def long(self):
+        max_ = len(sorted(self.words, key=len)[-1])
+        res = list(filter(lambda x:len(x) == max_, self.words))
+        res = " ".join(res)
+        return res
+
+
+
+add_str = MinMaxWordFinder()
+add_str.add_sentenses(" hgfdhsh sdf sdfe eassd t anklj;g")
+add_str.add_sentenses("hgfdhsh i 5 ")
+print(add_str.words)
+print(sorted(add_str.words, key=len))
+
+print(add_str.short())
+print(add_str.long())
